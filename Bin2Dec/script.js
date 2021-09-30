b0aceca39d8f306adc78e6ddd22189bd504e4388
ClaudiuSC <67117889+ClaudiuSC@users.noreply.github.com>
2021-09-30 12:28:32 +0300

    Add files via upload

---
+++ Bin2Dec/script.js
@@ -0,0 +1,33 @@
+const binInput = document.querySelector(".binary")
+const btn = document.querySelector("#btn")
+const result = document.querySelector("#result")
+
+binInput.addEventListener("keyup", checkInput)
+btn.addEventListener("click", () => {if(checkInput() != 0){result.textContent = binToDec(binInput.value)}})
+
+function binToDec(x) {
+    let counter = 0
+    let total = 0
+    for (let i=binInput.value.length-1; i>-1 ;i--) {
+        if ( binInput.value[i] == 1) {
+            total += 2**counter
+        }
+        counter++
+    }
+    return total    
+}
+
+function checkInput() {
+    const search = binInput.value
+    for (let i = 0; i < search.length; i++) {
+        if(search[i] != 0 && search[i] != 1) {
+            binInput.classList.add("show") 
+            return 0
+        } else if (search[i] == 0) {
+            binInput.classList.remove("show")
+        } else if (search[i] == 1) {
+            binInput.classList.remove("show") 
+        }
+    }
+}
+
